156cf4933a83fead8908e75ffea94fd33a69dc70
HARSH NAYAK <100467506+terMinator1812003@users.noreply.github.com>
2022-12-22 23:52:22 +0530

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class SelectionSort
+{
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[])
+    {
+        int arr[]={7,8,1,3,2};
+
+        //selection sort 
+        for(int i=0;i<arr.length-1;i++)
+        {
+            int smallest=i;
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[smallest]>arr[j]){
+                    smallest=j;
+                }
+            int temp=arr[smallest];
+            arr[smallest]=arr[i];
+            arr[i]=temp;    
+            }
+        }
+        printArray(arr);
+
+    }
+
+}
