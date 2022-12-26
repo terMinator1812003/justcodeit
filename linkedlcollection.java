0ee6cbaa111a3e5da704734eb6d6d1f27bf7b9c7
HARSH NAYAK <100467506+terMinator1812003@users.noreply.github.com>
2022-12-26 23:31:12 +0530

    Add files via upload

---
+++ linkedlcollection.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class linkedlcollection 
+{
+    public static void main(String[] args)
+    {
+        LinkedList <Integer> obj1=new LinkedList <Integer>();
+        obj1.add(9);
+        obj1.add(7);
+        obj1.add(5);
+        obj1.add(10);
+        System.out.println("linked list is :"+obj1);
+    }
+    
+}
