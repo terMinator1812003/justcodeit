14de538318b0ddf075a229a74933fbb03c95ee51
HARSH NAYAK <100467506+terMinator1812003@users.noreply.github.com>
2023-04-18 23:36:47 +0530

    Add files via upload

---
+++ Circle_01.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Circle {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+public class Circle_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circle: ");
+        double radius = scanner.nextDouble();
+
+        Circle circle = new Circle(radius);
+
+        System.out.println("Area of the circle is: " + circle.getArea());
+        System.out.println("Perimeter of the circle is: " + circle.getPerimeter());
+    }
+}
