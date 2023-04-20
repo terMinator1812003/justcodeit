3eda36b6caa1b7cd842841e42c3a4f4561c412fb
HARSH NAYAK <100467506+terMinator1812003@users.noreply.github.com>
2023-04-20 22:41:41 +0530

    Add files via upload

---
+++ Person01.java
@@ -0,0 +1,27 @@
+//JAVA bean for a Person
+
+public class Person01 {
+    // private varibales
+    private String firstName;
+    private String lastName;
+
+    // getters and setters for variables
+    public String getFirstName() {
+        return firstName;
+
+    }
+
+    public String getLastName() {
+        return lastName;
+
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
